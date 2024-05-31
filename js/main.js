763bbb21478d15b4710d7931b726289367aec813
Nana Qwesi Rex <65394125+Qwesi-Rex@users.noreply.github.com>
2024-05-31 15:28:13 +0000

    Initial

--- js/main.js
+++ js/main.js
@@ -87,7 +87,7 @@
 
 	var countDown = function() {
 
-		var d = new Date(new Date().getTime() + 800 * 120 * 120 * 2000);
+		var d = new Date(new Date().getTime() + 800 * 12 * 120 * 2000);
 		simplyCountdown('.simply-countdown-one', {
 			year: d.getFullYear(),
 			month: d.getMonth() - 7,
